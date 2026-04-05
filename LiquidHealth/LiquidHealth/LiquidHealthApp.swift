3d330e42ce068aba5a51256879c7431b75cbef52
amilcarjose9 <amilcarjpp@gmail.com>
2026-04-05 18:30:19 -0400

    add project files

---
+++ LiquidHealth/LiquidHealth/LiquidHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidHealthApp.swift
+//  LiquidHealth
+//
+//  Created by Amilcar Pena Jr on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct LiquidHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
